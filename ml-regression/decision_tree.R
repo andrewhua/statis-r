008d43302bb1e36386f682891fa1eb73ed239629
Andrew Hua <andrew.hua@icloud.com>
2017-04-02 13:28:58 +0930

    decision tree

--- ml-regression/decision_tree.R
+++ ml-regression/decision_tree.R
@@ -1,10 +1,14 @@
 source("cv.R")
 
-MSE = rep(0, Z)
-for(i in 1:Z){
+library(rpart.plot)
+a = rpart(y~., w)
+rpart.plot(a, type=2, faclen=T)
+
+MSE = rep(0,Z)
+for(i in 1:Z) {
     m = mm[[i]]
     M = mean((w[m,D] - mean(w[m,D]))^2)
-    a = lm(y~., w[-m,])
+    a = rpart(y~., w[-m,])
     MSE[i] = mean((w[m,D] - predict(a, w[m,]))^2)/M
 }
 mean(MSE)
