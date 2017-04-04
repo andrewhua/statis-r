9489839d1378a4b7ea931b41c123c069c655eb4e
Andrew Hua <andrew.hua@icloud.com>
2017-04-04 21:17:32 +0930

    appending ann

--- ml-regression/artificial_neural_networks.R
+++ ml-regression/artificial_neural_networks.R
@@ -1,4 +1,4 @@
-
+source("cv.R")
 
 require(nnet)
 require(caret)
@@ -7,3 +7,13 @@ mygrid = expand.grid(.decay=c(0.5,0.1), .size=c(4,5,6))
 nnetfit = train(y/max(w,[,D])~., data=w, method="nnet",
     maxit=1000, tuneGrid=mygrid, trace=F)
 print(nnetfit)
+
+MSE = rep(0,Z)
+set.seed(1010)
+for(i in 1:Z) {
+    m = mm[[i]]
+    M = mean((w[m,D] - mean(w[m,D]))^2)
+    a = nnet(y/max(w[,D])~., data=w[-m,], size=4, decay=0.1)
+    MSE[i] = mean((w[m,D] - predict(a, w[m,])*max(w[,D]))^2)/M
+}
+mean(MSE)
